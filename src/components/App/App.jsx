08537e65975d292f7de5ba23c7b7b30bd122c638
Adil Khan <adilkhan@Adils-MacBook-Air.local>
2026-05-04 23:06:36 -0500

    baseUrl_envLogic_update

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -62,7 +62,10 @@ function App() {
   const navigate = useNavigate();
   const location = useLocation();
 
-  const baseUrl = "http://localhost:3001";
+  const baseUrl =
+    process.env.NODE_ENV === "production"
+      ? "https://api.the-newsapp.blinklab.com"
+      : "http://localhost:3001";
 
   const closeActiveModal = () => {
     setActiveModal("");
